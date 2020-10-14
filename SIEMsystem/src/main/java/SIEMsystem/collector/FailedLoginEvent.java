6162ac845ddc4cbc3c6b8c761a08fdff30ad5bf6
nguynnhthi <59842995+nguynnhthi@users.noreply.github.com>
2020-10-14 08:24:03 +0700

    Add files via upload

---
+++ SIEMsystem/src/main/java/SIEMsystem/collector/FailedLoginEvent.java
@@ -0,0 +1,33 @@
+package SIEMsystem.collector;
+import lombok.Getter;
+import lombok.Setter;
+
+public class FailedLoginEvent {
+    @Getter @Setter private String ip;
+    @Getter @Setter private String user;
+    @Getter @Setter private String time;
+    @Getter @Setter private String method;
+    @Getter @Setter private String uri;
+    @Getter @Setter private String query;
+    @Getter @Setter private String protocol;
+    @Getter @Setter private String status;
+    @Getter @Setter private String bytes;
+    @Getter @Setter private String referer;
+    @Getter @Setter private String useragent;
+
+    public FailedLoginEvent(AccessLog al) {
+        this.ip = al.getIp();
+        this.user = al.getUser();
+        this.time = al.getTime();
+        this.method = al.getMethod();
+        this.uri = al.getUri();
+        this.query = al.getQuery();
+        this.protocol = al.getProtocol();
+        this.status = al.getStatus();
+        this.bytes = al.getBytes();
+        this.referer = al.getReferer();
+        this.useragent = al.getUseragent();
+        System.out.println("FailedLoginEvent created");
+        System.out.println();
+    }
+}
