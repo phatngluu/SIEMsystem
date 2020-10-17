a1bd5b4b05451026f029aae704ab68f06b62fa49
phatngluu <yadsendew@protonmail.com>
2020-10-17 12:31:51 +0700

    test gui in remote server

--- src/main/java/SIEMsystem/dashboard/Dashboard.java
+++ src/main/java/SIEMsystem/dashboard/Dashboard.java
@@ -1,21 +1,45 @@
 package SIEMsystem.dashboard;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Dashboard {
     public static void main(String[] args) {
         new Dashboard();
     }
-    public Dashboard(){
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+
+    public Dashboard() {
+        frame = new JFrame();
+
+        JButton button = new JButton("Click me");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                count++;
+                label.setText("Number of clicks: " + count);
+            }
+        });
+
+        label = new JLabel("Number of clicks: 0");
+
+        panel = new JPanel();
 
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0,1));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,4 +47,5 @@ public class Dashboard {
         frame.pack();
         frame.setVisible(true);
     }
+
 }
