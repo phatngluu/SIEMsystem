4cc94d2ec7720f19011dc41389815de517c2df4a
phatngluu <yadsendew@protonmail.com>
2020-10-29 15:29:59 +0700

    commit continued

--- src/main/java/SIEMsystem/alert/FailedLoginAlert.java
+++ src/main/java/SIEMsystem/alert/FailedLoginAlert.java
@@ -4,6 +4,6 @@ public class FailedLoginAlert extends Alert {
     public FailedLoginAlert(String ip, String time, String status) {
         super();
         this.name = this.getClass().getSimpleName();
-        this.setMessage("A failed login attempt from " + ip + " within 5 minutes detected.");
+        this.setMessage("A failed login attempt from " + ip + " detected.");
     }
 }
